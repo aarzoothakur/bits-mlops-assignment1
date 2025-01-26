4a80ae8a731496b2cbf677787d5743ecb8a4c259
Aarzoo Thakur <aarzoo.thakur@ymail.com>
2025-01-27 00:12:39 +0530

    Flask added

--- app.py
+++ app.py
@@ -8,16 +8,14 @@ app = Flask(__name__)
 # Load the trained model
 model = joblib.load('loan_prediction_tuned_model.joblib')
 
+
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.get_json(force=True)
-    
     # Prepare the input data for prediction
     features = np.array([data['features']])
-    
     # Predict the loan approval status
     prediction = model.predict(features)
-    
     # Return the result as a JSON response
     return jsonify({'prediction': prediction[0]})
 
